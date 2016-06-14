8dabaf03f0b1eaee594aecf55e3c52a19534f04e
hitechhitesh <hiteshj@swiftsetup.com>
2016-06-14 18:46:34 +0530

    third time commit

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,25 @@
+@extends('admin.layouts.app')
+
+@section('content')
+        <div class="col-md-10">
+            <div class="row">
+                <div class="col-md-10">
+                    <div class="content-box-large">
+                        <div class="panel-heading">
+                            <div class="panel-title">Latest posts</div>
+
+                            <div class="panel-options">
+                                <a href="#" data-rel="collapse"><i class="glyphicon glyphicon-refresh"></i></a>
+                                <a href="#" data-rel="reload"><i class="glyphicon glyphicon-cog"></i></a>
+                            </div>
+                        </div>
+                        <div class="panel-body">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -4,19 +4,18 @@
             <div class="sidebar content-box" style="display: block;">
                 <ul class="nav">
                     <!-- Main menu -->
-                    <li class="current"><a href="index.html"><i class="glyphicon glyphicon-home"></i> Dashboard</a></li>
+                    <li class="current"><a href="/admin"><i class="glyphicon glyphicon-home"></i> Dashboard</a></li>
                     <li class="submenu">
                         <a href="#">
-                            <i class="glyphicon glyphicon-list"></i> Pages
+                            <i class="glyphicon glyphicon-list"></i> Category
                             <span class="caret pull-right"></span>
                         </a>
                         <!-- Sub menu -->
                         <ul>
-                            <li><a href="login.html">Login</a></li>
+                            <li><a href="login.html">Add C</a></li>
                             <li><a href="signup.html">Signup</a></li>
                         </ul>
                     </li>
-
                 </ul>
             </div>
         </div>
